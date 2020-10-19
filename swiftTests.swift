76aefda7f4b615f6a9a91c4d2906797ccf8da58b
Pokemon Library <https://github.com/Denis85VV/PokemonLibrary.git>
2020-10-19 15:27:28 +0300

    add task 12

--- swiftTests.swift
+++ swiftTests.swift
@@ -44,3 +44,24 @@ if (name == "Anlexander" || name == "Denis" || name == "Emil") {
   print("Sorry, you are not in the list. See you at next time!")
 }
 
+// Модуль 12. Инициалиализировать массив из рандомных целых чисел. В цикле for вывести чётные числа, в цикле while нечётные.
+
+let numbers = [Int](1...100)
+var evenNumbers = [Int]() // четные числа
+var oddNumbers = [Int]() // не четные числа
+var oddNumbersCounter = 1
+
+for i in numbers {
+  if i % 2 == 0 {
+    evenNumbers.append(i)
+  }
+}
+print(evenNumbers)
+
+while oddNumbersCounter < 100 {
+    if numbers[oddNumbersCounter] % 2 == 0 {
+        oddNumbers.append(oddNumbersCounter)
+    }
+    oddNumbersCounter += 1
+}
+print(oddNumbers)
